5a5578d7efd2331f9f50581b9a2430e2dec7f621
jack <jack@arch.com>
2020-01-01 22:19:31 +0800

    11

--- examples/test.c
+++ examples/test.c
@@ -1,5 +1,6 @@
 #include "../lautoc.h"
 
+
 typedef struct {
   char* name;
   int num_wings;
@@ -11,22 +12,38 @@ birdie* get_instance_ptr(lua_State* L) {
   return &test_birdie;
 }
 
+int create_birdie(lua_State* L)
+{
+	birdie *ret = malloc(sizeof(birdie));
+	lua_pushlightuserdata(L, ret);
+	return 1;
+}
+
 int birdie_index(lua_State* L) {
   const char* membername = lua_tostring(L, -1);
-  birdie* self = get_instance_ptr(L);
 
-  lua_pushstring(L, "b");
+  lua_pushstring(L, "cdata");
   lua_gettable(L, -3);
-  int b = lua_tointeger(L, -1);
+  birdie* self = lua_touserdata(L, -1);
   lua_pop(L, 1);
-  printf("b = %d\n", b);
+
+//  lua_pushstring(L, "b");
+//  lua_gettable(L, -3);
+//  int b = lua_tointeger(L, -1);
+//  lua_pop(L, 1);
+//  printf("b = %d\n", b);
 
   return luaA_struct_push_member_name(L, birdie, membername, self);
 }
 
 int birdie_newindex(lua_State* L) {
   const char* membername = lua_tostring(L, -2);
-  birdie* self = get_instance_ptr(L);
+//  birdie* self = get_instance_ptr(L);
+  lua_pushstring(L, "cdata");
+  lua_gettable(L, -3);
+  birdie* self = lua_touserdata(L, -1);
+  lua_pop(L, 1);
+  
   luaA_struct_to_member_name(L, birdie, membername, self, -1);
   return 0;
 }
@@ -70,6 +87,9 @@ static int report (lua_State *L, int status) {
   return status;
 }
 
+int C(lua_State* L) {
+  return luaA_call_name(L, lua_tostring(L, 1));
+}
 
 int main(int argc, char **argv) {
   
@@ -79,7 +99,9 @@ int main(int argc, char **argv) {
   lua_State* L = luaL_newstate();
   luaL_openlibs(L);
   luaA_open(L);
-  
+
+  lua_register(L, "create_birdie", create_birdie);
+
   luaA_struct(L, birdie);
   luaA_struct_member(L, birdie, name, char*);
   luaA_struct_member(L, birdie, num_wings, int);
@@ -87,23 +109,32 @@ int main(int argc, char **argv) {
   lua_register(L, "birdie_index", birdie_index);
   lua_register(L, "birdie_newindex", birdie_newindex);
 
-/* (luaL_loadstring(L, str) || lua_pcall(L, 0, LUA_MULTRET, 0)) */
+  lua_register(L, "C", C);  
+
+    /* "C('hello_world')\n" */
   luaL_loadstring(L, ""
     "Birdie = {}\n"
     "setmetatable(Birdie, Birdie)\n"
     "function Birdie.__call()\n"
-    "  local self = {a = 10, b = 20, c = 30}\n"
+    "  local self = {cdata = nil, a = 10, b = 20, c = 30}\n"
+	"  self.cdata = create_birdie()\n"
     "  setmetatable(self, Birdie)\n"
     "  return self\n"
     "end\n"
     "Birdie.__index = birdie_index\n"
-    "Birdie.__newindex = birdie_newindex\n"
+    "-- Birdie.__newindex = birdie_newindex\n"
     "\n"
-    "bird = Birdie()\n"
-    "print(bird.name)\n"
-    "print(bird.num_wings)\n"
-    "bird.num_wings = 3\n"
-    "print(bird.num_wings)\n"
+    "bird1 = Birdie()\n"
+    "bird1.name = \"woshishui111\"\n"
+    "bird1.num_wings = 3\n"
+    "bird2 = Birdie()\n"
+    "bird2.name = \"woshishui222\"\n"
+    "bird2.num_wings = 22222\n"
+	  
+    "print(bird1.name)\n"
+    "print(bird1.num_wings)\n"
+    "print(bird2.name)\n"
+    "print(bird2.num_wings)\n"
     "\n");
 
   int status = docall(L, 0, LUA_MULTRET);
